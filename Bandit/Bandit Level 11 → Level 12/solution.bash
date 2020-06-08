6b381f50cd9ba9da582b9ccfecae3a8ccab1e920
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-08 13:36:48 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 11 → Level 12/solution.bash
@@ -0,0 +1,10 @@
+bandit11@bandit:~$ ls -al
+total 24
+drwxr-xr-x  2 root     root     4096 May  7 20:14 .
+drwxr-xr-x 41 root     root     4096 May  7 20:14 ..
+-rw-r--r--  1 root     root      220 May 15  2017 .bash_logout
+-rw-r--r--  1 root     root     3526 May 15  2017 .bashrc
+-rw-r-----  1 bandit12 bandit11   49 May  7 20:14 data.txt
+-rw-r--r--  1 root     root      675 May 15  2017 .profile
+bandit11@bandit:~$ cat data.txt | tr '[a-zA-Z]' '[n-za-mN-ZA-M]'
+The password is 5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu
