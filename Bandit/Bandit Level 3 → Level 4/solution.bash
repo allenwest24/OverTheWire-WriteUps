df69e0158fa6c68964d74e795cace993fbce131e
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-07 21:27:38 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 3 → Level 4/solution.bash
@@ -0,0 +1,17 @@
+bandit3@bandit:~$ ls -al
+total 24
+drwxr-xr-x  3 root root 4096 May  7 20:14 .
+drwxr-xr-x 41 root root 4096 May  7 20:14 ..
+-rw-r--r--  1 root root  220 May 15  2017 .bash_logout
+-rw-r--r--  1 root root 3526 May 15  2017 .bashrc
+drwxr-xr-x  2 root root 4096 May  7 20:14 inhere
+-rw-r--r--  1 root root  675 May 15  2017 .profile
+bandit3@bandit:~$ cd inhere
+bandit3@bandit:~/inhere$ ls
+bandit3@bandit:~/inhere$ ls -al
+total 12
+drwxr-xr-x 2 root    root    4096 May  7 20:14 .
+drwxr-xr-x 3 root    root    4096 May  7 20:14 ..
+-rw-r----- 1 bandit4 bandit3   33 May  7 20:14 .hidden
+bandit3@bandit:~/inhere$ cat .hidden
+pIwrPrtPN36QITSp3EQaw936yaFoFgAB
