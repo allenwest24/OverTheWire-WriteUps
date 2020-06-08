4cc737c21d054279566adcbf9cc9d51fe7b5f373
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-08 11:05:07 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 9 → Level 10/solution.bash
@@ -0,0 +1,20 @@
+bandit9@bandit:~$ ls -al
+total 40
+drwxr-xr-x  2 root     root     4096 May  7 20:14 .
+drwxr-xr-x 41 root     root     4096 May  7 20:14 ..
+-rw-r--r--  1 root     root      220 May 15  2017 .bash_logout
+-rw-r--r--  1 root     root     3526 May 15  2017 .bashrc
+-rw-r-----  1 bandit10 bandit9 19379 May  7 20:14 data.txt
+-rw-r--r--  1 root     root      675 May 15  2017 .profile
+bandit9@bandit:~$ strings data.txt | grep =
+========== the*2i"4
+=:G e
+========== password
+<I=zsGi
+Z)========== is
+A=|t&E
+Zdb=
+c^ LAh=3G
+*SF=s
+&========== truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk
+S=A.H&^
