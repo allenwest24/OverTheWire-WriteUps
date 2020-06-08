5c399df47497c380371612bca0e6c3bba1511214
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-07 23:09:10 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 4 → Level 5/solution.bash
@@ -0,0 +1,25 @@
+bandit4@bandit:~$ ls -al
+total 24
+drwxr-xr-x  3 root root 4096 May  7 20:14 .
+drwxr-xr-x 41 root root 4096 May  7 20:14 ..
+-rw-r--r--  1 root root  220 May 15  2017 .bash_logout
+-rw-r--r--  1 root root 3526 May 15  2017 .bashrc
+drwxr-xr-x  2 root root 4096 May  7 20:15 inhere
+-rw-r--r--  1 root root  675 May 15  2017 .profile
+bandit4@bandit:~$ cd inhere
+bandit4@bandit:~/inhere$ ls -al
+total 48
+drwxr-xr-x 2 root    root    4096 May  7 20:15 .
+drwxr-xr-x 3 root    root    4096 May  7 20:14 ..
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file00
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file01
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file02
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file03
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file04
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:14 -file05
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:15 -file06
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:15 -file07
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:15 -file08
+-rw-r----- 1 bandit5 bandit4   33 May  7 20:15 -file09
+bandit4@bandit:~/inhere$ cat ./-file07
+koReBOKuIDDepwhWk7jZC0RTdopnAYKh
