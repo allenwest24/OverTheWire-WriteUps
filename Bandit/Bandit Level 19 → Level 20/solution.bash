8614717a7e750a13ab1e68975b98defb7f04aa37
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-09 14:36:38 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 19 → Level 20/solution.bash
@@ -0,0 +1,13 @@
+bandit19@bandit:~$ ls -al
+total 28
+drwxr-xr-x  2 root     root     4096 May  7 20:14 .
+drwxr-xr-x 41 root     root     4096 May  7 20:14 ..
+-rwsr-x---  1 bandit20 bandit19 7296 May  7 20:14 bandit20-do
+-rw-r--r--  1 root     root      220 May 15  2017 .bash_logout
+-rw-r--r--  1 root     root     3526 May 15  2017 .bashrc
+-rw-r--r--  1 root     root      675 May 15  2017 .profile
+bandit19@bandit:~$ ./bandit20-do
+Run a command as another user.
+  Example: ./bandit20-do id
+bandit19@bandit:~$ ./bandit20-do cat /etc/bandit_pass/bandit20
+GbKksEFF4yrVs6il55v6gwY5aVje5f0j
