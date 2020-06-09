fda82aa6d20a7a1c0de9c98fb062fe6136134b29
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-09 14:28:17 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 18 → Level 19/solution.bash
@@ -0,0 +1,8 @@
+C:\Users\Allen>ssh bandit18@bandit.labs.overthewire.org -p 2220 -t /bin/sh
+This is a OverTheWire game server. More information on http://www.overthewire.org/wargames
+
+bandit18@bandit.labs.overthewire.org's password:
+$ cat readme
+IueksS7Ubh8G3DCwVzrTd8rAVOwq3M5x
+$ exit
+Connection to bandit.labs.overthewire.org closed.
