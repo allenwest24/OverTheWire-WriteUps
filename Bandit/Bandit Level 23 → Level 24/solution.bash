218d262bdc6357f1e8d3b3b76f10771cbefa8b55
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-11 17:09:47 -0400

    Create solution.bash

---
+++ Bandit/Bandit Level 23 → Level 24/solution.bash
@@ -0,0 +1,19 @@
+bandit23@bandit:~$ mkdir /tmp/allenwest24
+bandit23@bandit:~$ cd /tmp/allenwest24
+bandit23@bandit:/tmp/allenwest24$ vim bandit23.sh
+bandit23@bandit:/tmp/allenwest24$ chmod 777 bandit23.sh
+bandit23@bandit:/tmp/allenwest24$ touch bandit24pass
+bandit23@bandit:/tmp/allenwest24$ ls
+bandit23.sh  bandit24pass
+bandit23@bandit:/tmp/allenwest24$ chmod 777 bandit24pass
+bandit23@bandit:/tmp/allenwest24$ cp bandit23.sh /var/spool/bandit24/
+bandit23@bandit:/tmp/allenwest24$ cat bandit24pass
+.
+.
+.
+// Had to wait a minute
+.
+.
+.
+bandit23@bandit:/tmp/allenwest24$ cat bandit24pass
+UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ
