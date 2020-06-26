328508c8253917723f0aac43d42f6303d8f920e7
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-26 15:56:41 -0400

    Create solution.bash

---
+++ Leviathan/Leviathan Level 4 → Level 5/solution.bash
@@ -0,0 +1,24 @@
+leviathan4@leviathan:~$ ls -al
+total 24
+drwxr-xr-x  3 root root       4096 Aug 26  2019 .
+drwxr-xr-x 10 root root       4096 Aug 26  2019 ..
+-rw-r--r--  1 root root        220 May 15  2017 .bash_logout
+-rw-r--r--  1 root root       3526 May 15  2017 .bashrc
+-rw-r--r--  1 root root        675 May 15  2017 .profile
+dr-xr-x---  2 root leviathan4 4096 Aug 26  2019 .trash
+leviathan4@leviathan:~$ cd .trash
+leviathan4@leviathan:~/.trash$ ls -al
+total 16
+dr-xr-x--- 2 root       leviathan4 4096 Aug 26  2019 .
+drwxr-xr-x 3 root       root       4096 Aug 26  2019 ..
+-r-sr-x--- 1 leviathan5 leviathan4 7352 Aug 26  2019 bin
+leviathan4@leviathan:~/.trash$ ./bin
+01010100 01101001 01110100 01101000 00110100 01100011 01101111 01101011 01100101 01101001 00001010
+.
+.
+.
+// Went to a binary translator online and the result was the password.
+// leviathan5:Tith4cokei
+.
+.
+.
