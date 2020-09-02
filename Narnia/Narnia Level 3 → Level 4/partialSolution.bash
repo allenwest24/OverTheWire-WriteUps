a6bf23bd07b43b64aeee481c30c6624fd4f6e405
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-09-01 22:05:41 -0400

    Create partialSolution.bash

---
+++ Narnia/Narnia Level 3 → Level 4/partialSolution.bash
@@ -0,0 +1,77 @@
+narnia3@narnia:~$ cd ..
+narnia3@narnia:/home$ cd ..
+narnia3@narnia:/$ ls
+bin      etc             lib     lost+found  opt         run    sys  vmlinuz
+boot     home            lib32   media       proc        sbin   tmp  vmlinuz.old
+cgroup2  initrd.img      lib64   mnt         README.txt  share  usr
+dev      initrd.img.old  libx32  narnia      root        srv    var
+narnia3@narnia:/$ cd narnia
+narnia3@narnia:/narnia$ ls
+narnia0    narnia1    narnia2    narnia3    narnia4    narnia5    narnia6    narnia7    narnia8
+narnia0.c  narnia1.c  narnia2.c  narnia3.c  narnia4.c  narnia5.c  narnia6.c  narnia7.c  narnia8.c
+narnia3@narnia:/narnia$ cat narnia3.c
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+
+    int  ifd,  ofd;
+    char ofile[16] = "/dev/null";
+    char ifile[32];
+    char buf[32];
+
+    if(argc != 2){
+        printf("usage, %s file, will send contents of file 2 /dev/null\n",argv[0]);
+        exit(-1);
+    }
+
+    /* open files */
+    strcpy(ifile, argv[1]);
+    if((ofd = open(ofile,O_RDWR)) < 0 ){
+        printf("error opening %s\n", ofile);
+        exit(-1);
+    }
+    if((ifd = open(ifile, O_RDONLY)) < 0 ){
+        printf("error opening %s\n", ifile);
+        exit(-1);
+    }
+
+    /* copy from file1 to file2 */
+    read(ifd, buf, sizeof(buf)-1);
+    write(ofd,buf, sizeof(buf)-1);
+    printf("copied contents of %s to a safer place... (%s)\n",ifile,ofile);
+
+    /* close 'em */
+    close(ifd);
+    close(ofd);
+
+    exit(1);
+}
+.
+.
+.
+// I know I want to feed it a file that overflows the buffer but it's not working the way I want so I will revisit when I am smarter.
+.
+.
+.
